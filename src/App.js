a59926189e2b4844b0247d4337c05b5d028666bf
jairorjuela <genius85862@gmail.com>
2019-05-06 22:00:48 -0500

    forms

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Form />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
