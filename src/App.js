97d356d61b8ba45f4f1723ef390e198d45addd11
jairorjuela <genius85862@gmail.com>
2019-05-26 19:55:50 -0500

    Tarea 3

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Form from './Form.js';
-import Server from './Server.js'
-import Routerr from './Routerr.js'
-
-import { Button } from 'react-bootstrap';
+import posts from './posts'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Form />
-      <Server />
-      <Button variant="primary">Primary</Button>
-      <Routerr />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="filter">
+          <input type="text" placeholder="Ingresa el término de búsqueda" />
+        </div>
+        <ul>
+          <li>
+            <a href={posts[2].url} target="_blank">
+              <img src={posts[2].image} />
+            </a>
+            <p>{posts[2].title}</p>
+          </li>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/posts.js
@@ -0,0 +1,39 @@
+var posts = [
+  {
+    url: "https://medium.com/s/oversight/journalism-is-under-unprecedented-assault-99e9ce18316c",
+    title: "Journalism Is Under Unprecedented Assault",
+    image: "https://cdn-images-1.medium.com/max/2600/1*gYi1VWtS7eMd9Y7EIhiUaQ.jpeg"
+  },
+  {
+    url: "https://medium.com/javascript-in-plain-english/i-created-the-exact-same-app-in-react-and-vue-here-are-the-differences-e9a1ae8077fd",
+    title: "Same app in React and Vue",
+    image: "https://cdn-images-1.medium.com/max/1600/1*WRzDZndJCduHwqgOpWmbhQ.png"
+  },
+  {
+    url: "https://medium.com/unicorn-supplies/angular-vs-react-vs-vue-a-2017-comparison-c5c52d620176",
+    title: "Angular vs. React vs. Vue: A 2017 comparison",
+    image: "https://cdn-images-1.medium.com/max/1600/1*aPijhbTjT0VOxPYq2RkVUw.png"
+  },
+  {
+    url: "https://medium.freecodecamp.org/all-the-fundamental-react-js-concepts-jammed-into-this-single-medium-article-c83f9b53eac2",
+    title: "All the fundamental React.js concepts, jammed into this single Medium article",
+    image: "https://cdn-images-1.medium.com/max/1600/1*qUlxDdY3T-rDtJ4LhLGkEg.png"
+  },
+  {
+    url: "https://medium.freecodecamp.org/learn-react-js-in-5-minutes-526472d292f4",
+    title: "Learn React JS in 5 minutes — A tutorial for beginners",
+    image: "https://cdn-images-1.medium.com/max/1600/1*IfwvdXdue_6unCX5eCnnqg.png"
+  },
+  {
+    url: "https://medium.freecodecamp.org/the-react-handbook-b71c27b0a795",
+    title: "The React Handbook",
+    image: "https://cdn-images-1.medium.com/max/2400/1*m5aPLXkrWJs7xKsfYViJEg.png"
+  },
+  {
+    url: "https://medium.com/@addyosmani/progressive-web-apps-with-react-js-part-i-introduction-50679aef2b12",
+    title: "Progressive Web Apps with React.js",
+    image: "https://cdn-images-1.medium.com/max/1600/1*Ms2muRzG4DHE36YU4kX_ag@2x.png"
+  }
+];
+
+export default posts
