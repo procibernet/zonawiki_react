98712fb59ce941e75afb3227a6d9d5b670183506
jairorjuela <genius85862@gmail.com>
2019-06-17 23:27:58 -0500

    Introduction to Hooks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Form from './Form.js';
-import Server from './Server.js'
-import Routerr from './Routerr.js'
-
-import { Button } from 'react-bootstrap';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+
+  onClick(e) {
+      this.setState({
+          count: this.state.count + 1
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-      <Form />
-      <Server />
-      <Button variant="primary">Primary</Button>
-      <Routerr />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.onClick.bind(this)}>Incrementa</button>
       </div>
     );
   }
 }
+
+export default App;
+
+import React, { Fragment, useState } from 'react';
+
+export const App = () => {
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+      <h2>{count}</h2>
+      <button onClick={ () => setCount(count + 1) }>Incrementa</button>
+    </Fragment>
+  );
+};
 
 export default App;
